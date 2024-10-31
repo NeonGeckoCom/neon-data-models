fb3e0b8fc81562c1c7d338b5a903bf0d05f1d4e2
Daniel McKnight <daniel@neon.ai>
2024-10-30 18:37:04 -0700

    Fix syntax error in BaseModel definition

--- neon_data_models/base.py
+++ neon_data_models/base.py
@@ -4,6 +4,6 @@ from pydantic import ConfigDict, BaseModel as _BaseModel
 
 
 class BaseModel(_BaseModel):
-    allow_extra = "allow" if environ.get("NEON_DATA_MODELS_ALLOW_EXTRA",
-                                         False) else "ignore"
-    model_config = ConfigDict(extra=allow_extra)
+    _allow_extra = "allow" if environ.get("NEON_DATA_MODELS_ALLOW_EXTRA",
+                                          False) else "ignore"
+    model_config = ConfigDict(extra=_allow_extra)
