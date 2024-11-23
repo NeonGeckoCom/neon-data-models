0289609e3777d53978f8a4f9dbdfd2402f1d827a
Daniel McKnight <daniel@neon.ai>
2024-11-22 16:17:35 -0800

    Explicitly import submodules to troubleshoot model build errors

--- neon_data_models/models/__init__.py
+++ neon_data_models/models/__init__.py
@@ -23,3 +23,7 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from neon_data_models.models.user import *
+from neon_data_models.models.client import *
+from neon_data_models.models.api import *
