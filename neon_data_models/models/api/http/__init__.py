6b5ed0a8ff894b9c6f781d3f292e4116546308c2
Daniel McKnight <daniel@neon.ai>
2024-12-18 14:50:18 -0800

    Add missing `models.api.http` init file

--- neon_data_models/models/api/http/__init__.py
+++ neon_data_models/models/api/http/__init__.py
@@ -24,34 +24,4 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from typing import List, Optional
-from pydantic import Field
-
-from neon_data_models.models.base import BaseModel
-from neon_data_models.models.api.llm import BrainForgeLLM, LLMPersona, LLMRequest
-
-
-class LLMGetModelsHttpResponse(BaseModel):
-    models: List[BrainForgeLLM]
-
-
-class LLMGetPersonasHttpRequest(BaseModel):
-    model_id: str = Field(
-        description="Model ID (<name>@<version>) to get personas for")
-
-
-class LLMGetPersonasHttpResponse(BaseModel):
-    personas: List[LLMPersona] = Field(
-        description="List of personas associated with the requested model.")
-
-
-class LLMGetInferenceHttpRequest(LLMRequest):
-    llm_name: str = Field(description="Model name to request")
-    llm_revision: str = Field(description="Model revision to request")
-    model: Optional[str] = None
-
-
-__all__ = [LLMGetModelsHttpResponse.__name__,
-           LLMGetPersonasHttpRequest.__name__,
-           LLMGetPersonasHttpResponse.__name__,
-           LLMGetInferenceHttpRequest.__name__]
+from neon_data_models.models.api.http.brainforge import *
