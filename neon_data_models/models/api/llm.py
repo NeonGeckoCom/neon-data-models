95a73294d2fd9e2945cc098e53c0b3d3b8a02f8d
Daniel McKnight <daniel@neon.ai>
2024-12-19 14:30:40 -0800

    Prevent inserting `None` system prompt in completion request history

--- neon_data_models/models/api/llm.py
+++ neon_data_models/models/api/llm.py
@@ -181,8 +181,9 @@ class LLMRequest(BaseModel):
         history = self.messages[-2*self.max_history:]
         for msg in history:
             msg["role"] = mq2role.get(msg["role"]) or msg["role"]
-        history.insert(0, {"role": "system",
-                           "content": self.persona.system_prompt})
+        if self.persona.system_prompt:
+            history.insert(0, {"role": "system",
+                               "content": self.persona.system_prompt})
         history.append({"role": "user", "content": self.query})
         return {"model": self.model,
                 "messages": history,
