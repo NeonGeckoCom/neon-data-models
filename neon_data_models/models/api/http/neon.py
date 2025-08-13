fd2585fc383e1189f99e98fdf25b9d3eebe9c8fc
Daniel McKnight <daniel@neon.ai>
2025-08-13 13:17:43 -0700

    Refactor Skill API HTTP response to account for refactor in HANA

--- neon_data_models/models/api/http/neon.py
+++ neon_data_models/models/api/http/neon.py
@@ -24,7 +24,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from typing import Dict
+from typing import List
 from pydantic import Field, RootModel
 from neon_data_models.models.api.messagebus import (
     NeonSkillApiData,
@@ -33,55 +33,18 @@ from neon_data_models.models.api.messagebus import (
 )
 
 
+class NeonSkillApiHttpData(NeonSkillApiData):
+    skill_id: str = Field(
+            description="ID of the skill providing this API method")
+    api_method: str = Field(
+            description="API method name")
+
 class NeonHttpListSkillApiResponse(RootModel):
-    root: Dict[str, Dict[str, NeonSkillApiData]]
+    root: List[NeonSkillApiHttpData]
 
     model_config = {
         "json_schema_extra": {
             "examples": [
-                {
-                    "skill-about.neongeckocom": {
-                        "skill_info_examples": {
-                            "help": "API method to build a list of skill examples",
-                            "request_schema": None,
-                            "response_schema": None,
-                            "signature": "() -> List[str]",
-                            "type": "skill-about.neongeckocom.skill_info_examples",
-                        }
-                    },
-                    "skill-date_time.neongeckocom": {
-                        "get_current_time": {
-                            "help": "\n        Get the current timestamp in seconds since epoch.\n        :param request: Request containing location to get time of\n        :returns: Response containing current timestamp\n        ",
-                            "request_schema": {
-                                "properties": {
-                                    "location": {
-                                        "anyOf": [
-                                            {"type": "string"},
-                                            {"type": "null"},
-                                        ],
-                                        "default": None,
-                                        "title": "Location",
-                                    }
-                                },
-                                "title": "_CurrentTimeRequest",
-                                "type": "object",
-                            },
-                            "response_schema": {
-                                "properties": {
-                                    "current_timestamp": {
-                                        "title": "Current Timestamp",
-                                        "type": "number",
-                                    }
-                                },
-                                "required": ["current_timestamp"],
-                                "title": "_CurrentTimeResponse",
-                                "type": "object",
-                            },
-                            "signature": "(request: skill_date_time._CurrentTimeRequest) -> skill_date_time._CurrentTimeResponse",
-                            "type": "skill-date_time.neongeckocom.get_current_time",
-                        },
-                    },
-                }
             ]
         }
     }
